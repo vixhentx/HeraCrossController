890b0bdf122ead007364ec5e000830c406b21815
淼涛 温 <w1084349470@outlook.com>
2025-05-28 19:05:15 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HeraCrossController
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HeraCrossController
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
