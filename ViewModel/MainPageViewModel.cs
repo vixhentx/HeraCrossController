a04bc70ee1a633cc59368353c2a5a0db4d3bf209
淼涛 温 <w1084349470@outlook.com>
2025-05-28 22:24:38 +0800

    consolePannel绑定到consoleSwitch

--- ViewModel/MainPageViewModel.cs
+++ ViewModel/MainPageViewModel.cs
@@ -14,14 +14,10 @@ namespace HeraCrossController.ViewModel
         public string DataRecieved { get; set; }
         public string DataToSend { get; set; }
 
-        public bool IsConsoleExtended { get; set; }
-        public Visibility ConsoleVisibility => IsConsoleExtended ? Visibility.Visible : Visibility.Collapsed;
-
 
 
         public MainPageViewModel()
         {
-            IsConsoleExtended = false;
             DataRecieved ??= string.Empty;
             DataToSend ??= string.Empty;
         }
