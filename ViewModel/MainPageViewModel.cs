be60baafb8c35ed14e1d0c39ae9dbaf448fd1118
淼涛 温 <w1084349470@outlook.com>
2025-05-28 23:24:24 +0800

    Command

--- ViewModel/MainPageViewModel.cs
+++ ViewModel/MainPageViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HeraCrossController.ViewModel
 {
@@ -18,13 +19,38 @@ namespace HeraCrossController.ViewModel
         public string DataRecieved { get; set; }
         public string DataToSend { get; set; }
 
+        Command _connectCommand, _sendDataCommand;
 
+        [DoNotNotify]
+        public Command ConnectCommand => _connectCommand;
+        [DoNotNotify]
+        public Command SendDataCommand => _sendDataCommand;
 
         public MainPageViewModel()
         {
             ConnectionStatus = ConnectionStatusEnum.Disconnected;
             DataRecieved ??= string.Empty;
             DataToSend ??= string.Empty;
+
+            _connectCommand = new Command(
+                execute: () =>
+                {
+                    RefreshCanExecutes();
+                    throw new NotImplementedException();
+                }
+                );
+            _sendDataCommand = new Command(
+                execute: () =>
+                {
+                    throw new NotImplementedException();
+                },
+                canExecute: () => ConnectionStatus== ConnectionStatusEnum.Connected
+                );
+        }
+
+        void RefreshCanExecutes()
+        {
+            ConnectCommand.ChangeCanExecute();
         }
     }
 }
